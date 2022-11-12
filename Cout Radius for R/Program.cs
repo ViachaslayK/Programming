e0229e48b84cb8a977cfd7083937ed96a2a45ba4
Slava Krylov <grimgrig@mail.ru>
2022-11-12 21:52:51 +0300

    Add project files.

---
+++ Cout Radius for R/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+internal class Radius
+{
+    static void Main()
+    {
+        double radius;
+        const double area = 10;
+        radius = area / (2 * 3.14159265358979);
+        Console.WriteLine("Радиус ранен: "+ radius);
+    }
+}
---
+++ decimal/Program.cs
@@ -0,0 +1,16 @@
+﻿class Desimal
+{
+    static void Main()
+    {
+        decimal amount=1000.0M;
+        const decimal rate_of_return= 0.07M;
+        int years = 10;
+
+        Console.WriteLine("Первоначальные вожения: " + amount);
+        Console.WriteLine("Норма прибыли: " + rate_of_return);
+        Console.WriteLine("В течение " + years+"лет");
+        for (int i = 0; i < years; i++)
+            amount=amount+(amount*rate_of_return);
+        Console.WriteLine("Будущая сотимость равна $" + amount);
+    }
+}
