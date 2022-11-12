0bcb8f43b73c147d8c1ed141858894872891d683
Slava Krylov <grimgrig@mail.ru>
2022-11-12 21:52:51 +0300

    Add project files.

---
+++ DIstance from Eath to Sun/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Inches
+{
+    static void Main()
+    {
+        long miles;
+        long inches;
+
+        miles = 93000000;
+        inches = miles * 5280 * 12;
+
+        Console.WriteLine("Расстояние до Солнце: " + inches + " в дюймах.");
+    }
+}
---
+++ First Kod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+       Console.WriteLine("Hello, World!");
+       variable();
+       Circle();
+    }
+    static void variable()
+    {
+        int x;
+        int y;
+        x = 100;
+        Console.WriteLine("x = " + x);
+        y = x / 2;
+        Console.Write("y = ");
+        Console.WriteLine(y);
+        
+    }
+
+    static void Circle()
+    {
+        double radius = 5;
+        double area;
+
+        area = radius * radius * 3.1415;
+
+        Console.WriteLine("Площать круга = " + area);
+
+    }
+}
